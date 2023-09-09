c4a9e5ee07b8809f86598c10830941219967232c
Hasibul Asad <hasibulasad60@gmail.com>
2023-09-09 20:09:29 +0600

    new database added and work with list

---
+++ app/src/main/java/com/hasibulasad/homoeodoctor/Adapter/SystemRvAdapter.java
@@ -0,0 +1,54 @@
+package com.hasibulasad.homoeodoctor.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hasibulasad.homoeodoctor.Models.SystemModel;
+import com.hasibulasad.homoeodoctor.R;
+
+import java.util.ArrayList;
+
+public class SystemRvAdapter extends RecyclerView.Adapter<SystemRvAdapter.ViewHolder> {
+
+    Context context;
+    ArrayList<SystemModel> systemlist = new ArrayList<>();
+
+    public SystemRvAdapter(Context context, ArrayList<SystemModel> systemlist) {
+        this.context = context;
+        this.systemlist = systemlist;
+    }
+
+    @NonNull
+    @Override
+    public SystemRvAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(context).inflate(R.layout.system_list_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SystemRvAdapter.ViewHolder holder, int position) {
+        SystemModel model = systemlist.get(position);
+        holder.numbertv.setText(model.getNumber());
+        holder.nametv.setText(model.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return systemlist.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView numbertv, nametv;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            numbertv = itemView.findViewById(R.id.idnumbertv);
+            nametv = itemView.findViewById(R.id.idnametv);
+        }
+    }
+}
--- app/src/main/java/com/hasibulasad/homoeodoctor/AnatomyActivity.java
+++ app/src/main/java/com/hasibulasad/homoeodoctor/AnatomyActivity.java
@@ -3,12 +3,41 @@ package com.hasibulasad.homoeodoctor;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hasibulasad.homoeodoctor.Adapter.SystemRvAdapter;
+import com.hasibulasad.homoeodoctor.Models.SystemModel;
+
+import java.util.ArrayList;
 
 public class AnatomyActivity extends AppCompatActivity {
-
+    RecyclerView anatomyRv;
+    ArrayList<SystemModel> systemNameList = new ArrayList<>();
+    SystemRvAdapter systemadapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anatomy);
+
+        anatomyRv = findViewById(R.id.idanatomyRv);
+
+        systemNameList.add(new SystemModel(1,"Skeletal System"));
+        systemNameList.add(new SystemModel(2,"Muscular System"));
+        systemNameList.add(new SystemModel(3,"Nervous System"));
+        systemNameList.add(new SystemModel(4,"Respiratory System"));
+        systemNameList.add(new SystemModel(5,"Cardiovuscular System"));
+        systemNameList.add(new SystemModel(6,"Digestive System"));
+        systemNameList.add(new SystemModel(7,"Urinary System"));
+        systemNameList.add(new SystemModel(8,"Reproductive System"));
+        systemNameList.add(new SystemModel(9,"Endocrine System"));
+        systemNameList.add(new SystemModel(10,"Integumentary System"));
+        systemNameList.add(new SystemModel(11,"Lymphetic System"));
+
+
+        anatomyRv.setLayoutManager(new LinearLayoutManager(this));
+        systemadapter = new SystemRvAdapter(this,systemNameList);
+        anatomyRv.setAdapter(systemadapter);
+        anatomyRv.setHasFixedSize(true);
     }
 }
